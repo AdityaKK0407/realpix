fc88a4399a15a5854963fc90b30a74cf3f6bf53e
Aditya Krishna Kotikalapudi <adityakk2207@gmail.com>
2026-03-05 23:50:27 +0530

    Removed deleted import

--- src/middleware/rate_limiter.py
+++ src/middleware/rate_limiter.py
@@ -4,7 +4,6 @@ from redis.exceptions import RedisError
 
 from src.dependencies import get_redis, get_verify_sha
 from src.redis_client.rate_limiter import VerifyTokenResult, verify_rate_limiter_token
-from src.helpers import logger
 
 async def rate_limiter_middleware(
     x_ratelimit_token: str | None = Header(default=None),
