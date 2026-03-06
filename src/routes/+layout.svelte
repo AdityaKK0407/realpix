bde67c87d47ba32266225f78cd247dfc69898709
Aditya Krishna Kotikalapudi <adityakk2207@gmail.com>
2026-03-06 14:24:51 +0530

    Setup for seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,26 @@
 	import '$lib/css/utilities.css';
 </script>
 
+<svelte:head>
+    <meta name="title" content="RealPix - AI Deepfake Detection Tool">
+    <meta name="description" content="Detect deepfakes and verify authenticity of images. Upload upto 5 images and get instant AI-powered analysis to identify manipulated images.">
+
+    <meta name="keywords" content="deepfake detection, image verification, AI detection, fake image detector, photo authenticity, manipulated image detection" />
+
+    <link rel="canonical" href="https://fab-realpix.netlify.app" />
+
+    <meta property="org:type" content="website" />
+    <meta property="og:url" content="https://fab-realpix.netlify.app" />
+    <meta property="og:title" content="RealPix - AI Deepfake Detection Tool" />
+    <meta property="og:description" content="Detect deepfakes and verify the authenticity of images instantly with AI powered analysis to identify manipulated images." />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://fab-realpix.netlify.app" />
+    <meta property="twitter:title" content="RealPix - AI Deepfake Detection Tool" />
+    <meta property="twitter:description" content="Detect deepfakes and verify the authenticity of images instantly with AI powered analysis to identify manipulated images." />
+
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot" content="index, follow" />
+</svelte:head>
+
 <slot />
