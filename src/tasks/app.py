f8030e86a2af1dd3a8be6cd671ea6c0e43bfe389
Aditya Krishna Kotikalapudi <adityakk2207@gmail.com>
2026-03-28 09:06:37 +0530

    Fixed celery to not default to RabbitMQ

--- src/tasks/app.py
+++ src/tasks/app.py
@@ -1,21 +1,17 @@
 from celery import Celery
 
-task_queue: Celery = Celery()
+task_queue = Celery("src")
 
 
 def initialize_task_queue(host: str, port: int) -> None:
     global task_queue
-    task_queue = Celery(
-        main="src",
-        broker=f"redis://{host}:{port}/0",
-        backend=f"redis://{host}:{port}/0",
-        include=["src.tasks.model"],
-    )
-
     task_queue.conf.update(
+        broker_url=f"redis://{host}:{port}/0",
+        result_backend=f"redis://{host}:{port}/0",
         task_track_started=True,
         task_serializer="json",
         result_serializer="json",
         accept_content=["json"],
         worker_pool="solo",
+        include=["src.tasks.model"],
     )
